919446665e946d39678a92357291ceb33d0be649
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2011-12-17 10:23:49 +0400

    Export code from railwayjs

--- test/railway_routes_test.js
+++ test/railway_routes_test.js
@@ -1,5 +1,5 @@
 require('./spec_helper').init(module.exports);
-var pathetic = require('../lib/railway_routes');
+var routes = require('../lib/railway_routes');
 
 function fakeApp(container) {
     var app = {};
@@ -21,7 +21,7 @@ function fakeBridge() {
 
 it('should produce routes for all methods', function (test) {
     var paths = [];
-    var map = new pathetic.Map(fakeApp(paths), fakeBridge());
+    var map = new routes.Map(fakeApp(paths), fakeBridge());
     map.get('/signin', 'session#new');
     map.post('/signin', 'session#create');
     map.del('/signout', 'session#destroy');
@@ -37,7 +37,88 @@ it('should produce routes for all methods', function (test) {
         [ 'PUT', '/signup', 'users#create' ],
         [ 'ALL', '/path', 'controller#action' ]
     ]);
+    test.equal(map.pathTo.signin(), '/signin');
+    test.equal(map.pathTo.signin, '/signin');
+    test.equal(map.pathTo.signout(), '/signout');
+    test.equal(map.pathTo.signup(), '/signup');
+    test.equal(map.pathTo.path(), '/path');
     test.done();
 });
 
+it('should create resourceful routes', function (test) {
+    var paths = [];
+    var map = new routes.Map(fakeApp(paths), fakeBridge());
+    map.resources('users');
+    test.deepEqual(paths, [
+        [ 'GET', '/users.:format?', 'users#index' ],
+        [ 'POST', '/users.:format?', 'users#create' ],
+        [ 'GET', '/users/new.:format?', 'users#new' ],
+        [ 'GET', '/users/:id/edit.:format?', 'users#edit' ],
+        [ 'DELETE', '/users/:id.:format?', 'users#destroy' ],
+        [ 'PUT', '/users/:id.:format?', 'users#update' ],
+        [ 'GET', '/users/:id.:format?', 'users#show' ]
+    ]);
+    test.equal(map.pathTo.users(), '/users');
+    test.equal(map.pathTo.users, '/users');
+    test.equal(map.pathTo.new_user(), '/users/new');
+    test.equal(map.pathTo.new_user, '/users/new');
+    test.equal(map.pathTo.edit_user(1), '/users/1/edit');
+    test.equal(map.pathTo.user(2), '/users/2');
+    test.done();
+});
 
+it('should describe namespaced route', function (test) {
+    var paths = [];
+    var map = new routes.Map(fakeApp(paths), fakeBridge());
+    map.namespace('admin', function (admin) {
+        admin.get('dashboard', 'dashboard#index');
+    });
+    test.deepEqual(paths, [
+        [ 'GET', '/admin/dashboard', 'admin/dashboard#index' ]
+    ]);
+    test.equal(map.pathTo.admin_dashboard(), '/admin/dashboard');
+    test.done();
+});
+
+it('should describe namespaced resource', function (test) {
+    var paths = [];
+    var map = new routes.Map(fakeApp(paths), fakeBridge());
+    map.namespace('admin', function (admin) {
+        admin.resources('pages');
+    });
+    test.deepEqual(paths, [
+        [ 'GET', '/admin/pages.:format?', 'admin/pages#index' ],
+        [ 'POST', '/admin/pages.:format?', 'admin/pages#create' ],
+        [ 'GET', '/admin/pages/new.:format?', 'admin/pages#new' ],
+        [ 'GET', '/admin/pages/:id/edit.:format?', 'admin/pages#edit' ],
+        [ 'DELETE', '/admin/pages/:id.:format?', 'admin/pages#destroy' ],
+        [ 'PUT', '/admin/pages/:id.:format?', 'admin/pages#update' ],
+        [ 'GET', '/admin/pages/:id.:format?', 'admin/pages#show' ]
+    ]);
+    test.equal(map.pathTo.admin_pages, '/admin/pages');
+    test.equal(map.pathTo.new_admin_page(), '/admin/pages/new');
+    test.equal(map.pathTo.new_admin_page, '/admin/pages/new');
+    test.equal(map.pathTo.edit_admin_page(1), '/admin/pages/1/edit');
+    test.equal(map.pathTo.admin_page(2), '/admin/pages/2');
+    test.done();
+});
+
+it('should allow overwrite path and helper', function (test) {
+    var paths = [];
+    var map = new routes.Map(fakeApp(paths), fakeBridge());
+    map.resources('avatars', {as: 'images', path: 'pictures'});
+    test.deepEqual(paths, [
+        [ 'GET', '/pictures.:format?', 'avatars#index' ],
+        [ 'POST', '/pictures.:format?', 'avatars#create' ],
+        [ 'GET', '/pictures/new.:format?', 'avatars#new' ],
+        [ 'GET', '/pictures/:id/edit.:format?', 'avatars#edit' ],
+        [ 'DELETE', '/pictures/:id.:format?', 'avatars#destroy' ],
+        [ 'PUT', '/pictures/:id.:format?', 'avatars#update' ],
+        [ 'GET', '/pictures/:id.:format?', 'avatars#show' ]
+    ]);
+    test.equal(map.pathTo.pictures, '/pictures');
+    test.equal(map.pathTo.new_picture, '/pictures/new');
+    test.equal(map.pathTo.edit_picture(1), '/pictures/1/edit');
+    test.equal(map.pathTo.picture(1602), '/pictures/1602');
+    test.done();
+});
